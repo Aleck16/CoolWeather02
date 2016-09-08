9ef9f278350dcd716f57a37429bffd2303470225
Aleck <2292092213@qq.com>
2016-09-08 10:19:34 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/app/coolweather/com/coolweather02/model/Province.java
@@ -0,0 +1,34 @@
+package app.coolweather.com.coolweather02.model;
+
+/**
+ * Created by Aleck_ on 2016/9/7.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
